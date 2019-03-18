42cb2316765bda544684ff17d619a8bef06051d6
Chris Chan <moming2k@gmail.com>
2019-03-18 17:48:30 +0800

    loop local folder

--- script/extract_csv_to_json.js
+++ script/extract_csv_to_json.js
@@ -1,2 +1,18 @@
+var fs = require('fs');
+var path = require('path');
+
 console.log("start");
 
+// Loop through all the files in the temp directory
+fs.readdir("japanmemo", function (err, files) {
+    if (err) {
+        console.error("Could not list the directory.", err);
+        process.exit(1);
+    }
+  
+    files.forEach(function (file, index) {
+        // Make one pass and make the file complete
+        var fromPath = path.join("japanmemo", file);
+        console.log("fromPath = " + fromPath);
+    });
+});
