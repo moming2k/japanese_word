e81bf1b60fd0fe02cd00afa17594a426bd79664e
Chris Chan <moming2k@gmail.com>
2019-03-19 17:26:55 +0800

    clean code

--- script/extract_csv_to_json.js
+++ script/extract_csv_to_json.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-var csvjson = require('csvjson');
-var convert_jp_txt_to_json = require('./japanmemo/convert_txt_to_json');
+const csvjson = require('csvjson');
+const convert_jp_txt_to_json = require('./japanmemo/convert_txt_to_json');
 
 console.log("start");
 
@@ -35,48 +35,6 @@ const start = async () => {
         console.log("after = " +toPath)
     
     }
-        // if (err) {
-        //     console.error("Could not list the directory.", err);
-        //     process.exit(1);
-        // }
-    
-        // const toFolder = "target/japanmemo/"
-        
-        // await convert_jp_txt_to_json("","");
-        // files.forEach(function (file, index) {
-        //     const fromPath = path.join("japanmemo", file);
-        //     if (file.endsWith(".txt")) {
-        //         to_file = file.substr(0, file.lastIndexOf(".")) + ".json";
-        //         let toPath = path.join(toFolder, to_file);
-
-        //         await convert_jp_txt_to_json(fromPath, toPath)
-        //         // fs.stat(fromPath, function (error, stat) {
-        //         //     if (error) {
-        //         //       console.error("Error stating file.", error);
-        //         //       return;
-        //         //     }
-                
-        //         //     if (stat.isFile()) {
-        //         //         console.log("fromPath = " + fromPath);
-
-        //         //         
-
-        //         //         var options = {
-        //         //             delimiter   : "=",
-        //         //             wrap        : false,
-        //         //             headers     : "none"
-        //         //         };
-
-        //         //         var read = fs.createReadStream(fromPath);
-        //         //         var write = fs.createWriteStream(toPath);
-        //         //         var toObject = csvjson.stream.toArray(options);
-        //         //         var stringify = csvjson.stream.stringify();
-        //         //         read.pipe(toObject).pipe(stringify).pipe(write);
-        //         //     }
-        //         // });
-        //     }
-        // });
-    // });
 }
 
 start()
