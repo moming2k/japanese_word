36aa197cc58341b4eddbc5d01b563f4856f38dc8
Chris Chan <moming2k@gmail.com>
2019-03-21 17:05:31 +0800

    improve score of the PWA

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,6 +1,7 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
     mode: 'development',
@@ -24,6 +25,25 @@ module.exports = {
     },
     plugins: [
         new CopyWebpackPlugin(['index.html']),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new WebpackPwaManifest({
+            name: 'Japanese Learning App',
+            short_name: 'JLA',
+            description: 'Learn Japanese as Game!',
+            background_color: '#ffffff',
+            crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            fingerprints: false,
+            theme_color: '#2F3BA2',
+            icons: [
+              {
+                src: path.resolve('src/assets/icon.png'),
+                sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+              },
+              {
+                src: path.resolve('src/assets/large-icon.png'),
+                size: '1024x1024' // you can also use the specifications pattern
+              }
+            ]
+        })
     ]
 };
